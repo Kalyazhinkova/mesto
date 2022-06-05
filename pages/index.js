7ec70d56d49e602b7b46b97a655c50cad0b33c60
Olga Kalyazhinkova <oi.kalyazhinkova@yandex.ru>
2022-06-06 00:04:35 +0500

    День 5

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
+const popup = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = document.querySelector('.popup__close-button');
-const popup = document.querySelector('.popup');
+const buttonSave = document.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
-buttonEdit.addEventListener('click', function(){
-  popup.classList.remove('popup_hidden')
-}) 
+let nameInput = document.querySelector('.form__item-name');
+let jobInput = document.querySelector('.form__item-description');
 
-buttonClose.addEventListener('click', function(){
-  popup.classList.add('popup_hidden')
-})
+function openPopup(){
+  popup.classList.remove('popup_hidden');
+}
+
+function closePopup(){
+  popup.classList.add('popup_hidden');
+}
+
+
+buttonEdit.addEventListener('click', openPopup);
+
+buttonClose.addEventListener('click', closePopup);
+
+let formElement = document.querySelector('.form');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
